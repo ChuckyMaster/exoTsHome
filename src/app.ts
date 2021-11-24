20851f9306d65d0fab1fa8b4426d70e638d846db
Chucky Master <user.email>
2021-11-24 23:56:02 +0100

    initialisation projet

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
 // Code goes here!
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDListElement;
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDListElement;
+
+    const importeNode = document.importNode(this.templateElement.content, true);
+  }
+}
